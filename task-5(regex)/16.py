b6c333bfdc13001539be9a546285d790b0e450b5
Hema Singani <107752501+Hemasingani@users.noreply.github.com>
2022-08-08 01:55:06 +0530

    Add files via upload

---
+++ task-5(regex)/16.py
@@ -0,0 +1,2 @@
+regex_integer_in_range = r"^[1-9][0-9]{5}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(?=((\d)\d\2))"	# Do not delete 'r'.
---
+++ task-5(regex)/6.py
@@ -0,0 +1,6 @@
+import re
+S=int(input())
+for i in range(S):
+      pattern1=re.compile(r'(?<= )(&&)(?= )')
+      pattern2=re.compile(r'(?<= )(\|\|)(?= )')
+      print(pattern2.sub('or', pattern1.sub('and', input())))
